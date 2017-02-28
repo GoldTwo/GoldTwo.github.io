15ab53d8928df403b069dcbb725a124f064bb3b0
GoldTwo <goldtwo1298@gmail.com>
2017-02-28 12:24:45 -0700

    I changed a file name and I don't know how to add just that

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,9 +1,16 @@
 //Nothing much here at the moment but will eventully be the heart of the team pairing
 
 //Table Add rows
-var tablerow = document.createElement("div class='rTableRow'");
-var tablecell = document.createElement("div class='rTableCell'");
-var tabel = document.getElementById("maintable");
 
-tablerow.appendChild(tablecell);
+//Assigns Variable to a Table Row div
+var tablerow = document.createElement("div");
+//Assigns Variable to a Table Cell div
+var tablecell_1 = document.createElement("div");
+var tablecell_2 = document.createElement("div");
+//Assigns Variable for easier dom 
+var table = document.getElementById("maintable");
+
+tablerow.appendChild(tablecell_1);
+tablerow.appendChild(tablecell_2);
 table.appendChild(tablerow)
+
