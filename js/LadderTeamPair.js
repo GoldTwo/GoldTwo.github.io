9c926181646e268538405a1cce306cef33cfe904
GoldTwo <goldtwo1298@gmail.com>
2017-02-28 11:47:19 -0700

    Previous Commit failed so readded everything

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,1 +1,9 @@
 //Nothing much here at the moment but will eventully be the heart of the team pairing
+
+//Table Add rows
+var tablerow = document.createElement("div class='rTableRow'");
+var tablecell = document.createElement("div class='rTableCell'");
+var tabel = document.getElementById("maintable");
+
+tablerow.appendChild(tablecell);
+table.appendChild(tablerow)
