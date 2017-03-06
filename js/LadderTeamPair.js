6f1dc7b1dc778f7f8202882e2e781fad2f9a8136
GoldTwo <goldtwo1298@gmail.com>
2017-03-06 12:15:38 -0700

    Finally got the class to apply to the element created in dom

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,17 +1,21 @@
-//Nothing much here at the moment but will eventully be the heart of the team pairing
+//###Table Add rows###
 
-//Table Add rows
-
-//Assigns Variable to a Table Row div
+//Assigns Variables for Easy Dom
 var tablerow = document.createElement("div");
-//Assigns Variable to a Table Cell div
 var tablecell_1 = document.createElement("div");
 var tablecell_2 = document.createElement("div");
-//Assigns Variable for easier dom 
+var Cell_1Data = document.createTextNode("Cell 1 created with dom");
+var Cell_2Data = document.createTextNode("Cell 2 created with dom");
 var table = document.getElementById("maintable");
 
-//Dom appendChild's to add the elements 
-tablerow.appendChild(tablecell_1).className("rTableCell");
-tablerow.appendChild(tablecell_2).className("rTableCell");
-table.appendChild(tablerow).className("rTableRow");
+//Dom appendChild's to move the elements
+tablecell_1.appendChild(Cell_1Data);
+tablecell_2.appendChild(Cell_2Data);
+tablerow.appendChild(tablecell_1);
+tablerow.appendChild(tablecell_2);
+table.appendChild(tablerow);
 
+//Give it classes so it will become part of the table
+tablerow.className = "rTableRow";
+tablecell_1.className = "rTableCell";
+tablecell_2.className = "rTableCell";
