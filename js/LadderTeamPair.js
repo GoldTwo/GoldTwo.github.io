5118ad702d5c58351db773092c6b06ef9dcc4aa9
GoldTwo <goldtwo1298@gmail.com>
2017-03-06 12:26:23 -0700

    .

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,21 +1,26 @@
 //###Table Add rows###
 
-//Assigns Variables for Easy Dom
+var teams = ["Ben/Tom", "Andy/Tim", "Grey/Brady"];
+
+//Assigns Variables for adding the names to html with dom
 var tablerow = document.createElement("div");
 var tablecell_1 = document.createElement("div");
 var tablecell_2 = document.createElement("div");
-var Cell_1Data = document.createTextNode("Cell 1 created with dom");
-var Cell_2Data = document.createTextNode("Cell 2 created with dom");
+var Cell_1Data = document.createTextNode("A Cell");
+var Cell_2Data = document.createTextNode("B Cell");
 var table = document.getElementById("maintable");
 
-//Dom appendChild's to move the elements
-tablecell_1.appendChild(Cell_1Data);
-tablecell_2.appendChild(Cell_2Data);
-tablerow.appendChild(tablecell_1);
-tablerow.appendChild(tablecell_2);
-table.appendChild(tablerow);
+for (t = 0; t < teams.length; t++){
+	//Dom appendChild's to move the elements
+	tablecell_1.appendChild(Cell_1Data);
+	tablecell_2.appendChild(Cell_2Data);
+	tablerow.appendChild(tablecell_1);
+	tablerow.appendChild(tablecell_2);
+	table.appendChild(tablerow);
 
-//Give it classes so it will become part of the table
-tablerow.className = "rTableRow";
-tablecell_1.className = "rTableCell";
-tablecell_2.className = "rTableCell";
+	//Give it classes so it will become part of the table
+	tablerow.className = "rTableRow";
+	tablecell_1.className = "rTableCell";
+	tablecell_2.className = "rTableCell";
+}
+
