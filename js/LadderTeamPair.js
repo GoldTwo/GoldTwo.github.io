8031c069c5d4b67d8970bfbc6ebc921f5de1fff4
GoldTwo <goldtwo1298@gmail.com>
2017-03-07 13:06:41 -0700

    Added for loop that pulls from an array

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,16 +1,30 @@
 //###Table Add rows###
 
-var teams = ["Ben/Tom", "Andy/Tim", "Grey/Brady"];
+var team_1 = ["Ben/Tom", "Andy/Tim", "Grey/Brady"];
+var team_2 = ["Jerry/Jay", "Paul/Kyle", "Linus/Luke"];
+var rowcount = team_1.length;
+
+/*
+var array = [1, 2, 3, 56, 98];
+var x = 0;
+for(i = 0; i = array.length; i++){
+	if (array[i] < x){
+		x = i;
+	}
+}
+console.log(x);
+*/
 
 //Assigns Variables for adding the names to html with dom
-var tablerow = document.createElement("div");
-var tablecell_1 = document.createElement("div");
-var tablecell_2 = document.createElement("div");
-var Cell_1Data = document.createTextNode("A Cell");
-var Cell_2Data = document.createTextNode("B Cell");
-var table = document.getElementById("maintable");
 
-for (t = 0; t < teams.length; t++){
+for (t = 0; t < rowcount; t++){
+	var tablerow = document.createElement("div");
+	var tablecell_1 = document.createElement("div");
+	var tablecell_2 = document.createElement("div");
+	var table = document.getElementById("maintable");
+	var Cell_1Data = document.createTextNode(team_1[t]);
+	var Cell_2Data = document.createTextNode(team_2[t]);
+
 	//Dom appendChild's to move the elements
 	tablecell_1.appendChild(Cell_1Data);
 	tablecell_2.appendChild(Cell_2Data);
@@ -23,4 +37,3 @@ for (t = 0; t < teams.length; t++){
 	tablecell_1.className = "rTableCell";
 	tablecell_2.className = "rTableCell";
 }
-
