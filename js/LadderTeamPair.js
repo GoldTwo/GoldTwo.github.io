b4851263fcf116780b5985f6e5aca8495ea13ae7
GoldTwo <goldtwo1298@gmail.com>
2017-03-08 12:44:24 -0700

    Changed data bank to array with objects and half working input

--- js/LadderTeamPair.js
+++ js/LadderTeamPair.js
@@ -1,8 +1,22 @@
 //###Table Add rows###
 
-var team_1 = ["Ben/Tom", "Andy/Tim", "Grey/Brady"];
-var team_2 = ["Jerry/Jay", "Paul/Kyle", "Linus/Luke"];
-var rowcount = team_1.length;
+//var team_1 = ["Ben/Tom", "Andy/Tim", "Grey/Brady"];
+//var team_2 = ["Jerry/Jay", "Paul/Kyle", "Linus/Luke"];
+//var rowcount = team_1.length;
+
+var teams = [
+	{team_1: "Ben/Tom",
+	team_2: "Jerry/Jay"},
+	
+	{team_1: "Andy/Tim",
+	team_2: "Paul/Kyle"},
+	
+	{team_1: "Grey/Brady",
+	team_2: "Linus/Luke"},
+	
+	{team_1: "Edzel/Taran",
+	team_2: "Destin/Kurzgesagt"}
+]
 
 /*
 var array = [1, 2, 3, 56, 98];
@@ -17,13 +31,13 @@ console.log(x);
 
 //Assigns Variables for adding the names to html with dom
 
-for (t = 0; t < rowcount; t++){
+for (t = 0; t < teams.length; t++){
 	var tablerow = document.createElement("div");
 	var tablecell_1 = document.createElement("div");
 	var tablecell_2 = document.createElement("div");
 	var table = document.getElementById("maintable");
-	var Cell_1Data = document.createTextNode(team_1[t]);
-	var Cell_2Data = document.createTextNode(team_2[t]);
+	var Cell_1Data = document.createTextNode(teams[t].team_1);
+	var Cell_2Data = document.createTextNode(teams[t].team_2);
 
 	//Dom appendChild's to move the elements
 	tablecell_1.appendChild(Cell_1Data);
